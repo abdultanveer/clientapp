6619f05f4e097fa202e28f499a65cf8e512e90f2
Abdul <abdul.tanveer@gmail.com>
2023-11-02 11:34:46 +0800

    insert, query custom content provider present in motorolademos app

--- app/src/main/java/com/example/clientapp/MainActivity.kt
+++ app/src/main/java/com/example/clientapp/MainActivity.kt
@@ -1,33 +1,58 @@
 package com.example.clientapp
 
 import android.content.ComponentName
-import android.content.Context
+import android.content.ContentValues
 import android.content.Intent
 import android.content.ServiceConnection
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
+import android.os.Message
+import android.os.Messenger
+import android.os.RemoteException
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.clientapp.databinding.ActivityMainBinding
 import com.example.motorolademos.IAddInterface
+
 
 var TAG = "MainActivityClientapp"
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     var iRemoteService: IAddInterface? = null
+    private var serverMessenger: Messenger? = null
+    private var clientMessenger: Messenger? = null
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.btnShowDetails.setOnClickListener { showDetails() }
         binding.btnCalendar.setOnClickListener { openMotoCalendar() }
         binding.btnAdd.setOnClickListener { bindAddService() }
+        binding.btnSendmessage.setOnClickListener { bindAddService() }
+        binding.btnInsert.setOnClickListener { insertContentProvider() }
+        binding.btnRetreive.setOnClickListener { loadDataContentprovider() }
     }
 
+    private fun showDetails() {
+        val CONTENT_URI = Uri.parse("content://com.demo.user.provider/users")
+       var cursor = contentResolver.query(CONTENT_URI,null,null,null,null)
+       cursor?.moveToFirst()
+        var idIndex = cursor?.getColumnIndex("id")
+        var nameIndex = cursor?.getColumnIndex("name")
+        binding.tvSum.text = cursor?.getString(nameIndex!!)
+    }
+
+
     private fun bindAddService() {
+        clientMessenger = Messenger(handler)
+
         Log.i(TAG, "bindAddService method")
         val intent = Intent("ineed.addition.moto")
        // intent.setPackage("com.example.motorolademos")
@@ -46,11 +71,10 @@ class MainActivity : AppCompatActivity() {
 
 
     private val connection = object : ServiceConnection {
-        override fun onServiceConnected(p0: ComponentName?, aBinder: IBinder?) {
+        override fun onServiceConnected(p0: ComponentName?, msgBinder: IBinder?) {
             Log.i("clientActivity", "client activity  connected to service")
-            iRemoteService = IAddInterface.Stub.asInterface(aBinder)
-            var sum = iRemoteService?.add(30, 40)
-            binding.tvSum.text = sum.toString()
+            serverMessenger = Messenger(msgBinder)
+            sendMessageToServer()
 
         }
 
@@ -58,6 +82,45 @@ class MainActivity : AppCompatActivity() {
         override fun onServiceDisconnected(p0: ComponentName?) {
             TODO("Not yet implemented")
         }
+
+    }
+
+    var handler: Handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            // Update UI with remote process info
+            val bundle = msg.data
+            binding.tvSum.text = bundle.getInt("sum").toString()
+        }
+    }
+    private fun sendMessageToServer() {
+        val message = Message.obtain(handler)
+        val bundle = Bundle()
+        bundle.putString("clientmsg","client says hello")
+        message.data = bundle
+        message.replyTo = clientMessenger // we offer our Messenger object for communication to be two-way
+        try {
+            serverMessenger?.send(message)
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+        } finally {
+            message.recycle()
+        }
+    }
+
+    private fun loadDataContentprovider() {
+        val usersUri = Uri.parse("content://com.moto.usersdb")
+        var cursor = contentResolver.query(usersUri,null,null,null,null,)
+        cursor?.moveToFirst()
+        var nameIndex = cursor?.getColumnIndex("name")
+        var name =  cursor?.getString(nameIndex!!)
+        binding.tvSum.text = name
+    }
+
+    private fun insertContentProvider() {
+        val usersUri = Uri.parse("content://com.moto.usersdb")
+        val values = ContentValues()
+        values.put("name",binding.etName.text.toString())
+        contentResolver.insert(usersUri,values)
 
     }
 }
